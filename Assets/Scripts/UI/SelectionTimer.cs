19571baf6c343401c2a2284b73ef5ebe18ef0084
Eric Cook <jcook83@gatech.edu>
2015-10-23 02:37:14 -0400

    Beginning Selection Timer

---
+++ Assets/Scripts/UI/SelectionTimer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace Assets.Scripts.UI
+{
+    public class SelectionTimer : MonoBehaviour
+    {
+        public float timeToSelect = 10f;
+        private float timer = 0;
+
+        private float xScale = 1f;
+
+        private Image timerBar;
+
+        void Start()
+        {
+            timerBar = GameObject.Find("Card Selection Timer").GetComponent<Image>();
+        }
+
+        void Update()
+        {
+            timer += Time.deltaTime;
+
+            if(timer >= timeToSelect)
+            {
+                timer = 0f;
+
+            }
+        }
+    }
+}

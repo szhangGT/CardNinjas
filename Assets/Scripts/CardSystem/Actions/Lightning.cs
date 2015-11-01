7bad4dfd122ce92ff5e8dcf138ee0e0b97d82239
Brian Kalish <bkalish42@gmail.com>
2015-11-01 16:20:40 -0500

    Fireball Update and Lightning Card
    I couldn't figure out how to get the oil to work, so I just updated
    Drew's fireball to pierce and have the fire element (with his
    permission) and then I made a set of lightning cards that randomly hit
    enemy spaces. The more lightning bolts there are, the less damage the
    individual bolt does, but more damage overall.

---
+++ Assets/Scripts/CardSystem/Actions/Lightning.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using Assets.Scripts.Player;
+using Assets.Scripts.Grid;
+
+namespace Assets.Scripts.CardSystem.Actions
+{
+    class Lightning : Action
+    {
+        public override void useCard(Character actor)
+        {
+            GameObject[] strikenNodes = new GameObject[range];
+            int randHolder;
+            int strikeTracker = 0;
+            bool repeater = false;
+            if (actor.Direction == Util.Enums.Direction.Left)
+            {
+                GameObject[] enemyNodes = GameObject.FindGameObjectsWithTag("Red");
+                while (strikenNodes[range-1] == null)
+                {
+                    randHolder = (int)Random.Range(0, enemyNodes.Length);
+                    foreach(GameObject node in strikenNodes)
+                    {
+                        repeater = repeater||(node == enemyNodes[randHolder]);
+                    }
+                    if (repeater == false)
+                    {
+                        strikenNodes[strikeTracker] = enemyNodes[randHolder];
+                        strikeTracker++;
+                    }
+                    else
+                    {
+                        repeater = false;
+                    }
+                }
+                foreach(GameObject node in strikenNodes)
+                {
+                    spawnObjectUsingPrefabAsModel(damage, 9, .2f, false, Util.Enums.Direction.None, 10, 0, true, node.GetComponent<GridNode>(), actor);
+                }
+            }
+            if (actor.Direction == Util.Enums.Direction.Right)
+            {
+                GameObject[] enemyNodes = GameObject.FindGameObjectsWithTag("Blue");
+                while (strikenNodes[range-1] == null)
+                {
+                    randHolder = (int)Random.Range(0, enemyNodes.Length);
+                    Debug.Log(randHolder);
+                    foreach (GameObject node in strikenNodes)
+                    {
+                        repeater = repeater||(node == enemyNodes[randHolder]);
+                    }
+                    if (repeater == false)
+                    {
+                        strikenNodes[strikeTracker] = enemyNodes[randHolder];
+                        strikeTracker++;
+                    }
+                    else
+                    {
+                        repeater = false;
+                    }
+                }
+                foreach (GameObject node in strikenNodes)
+                {
+                    spawnObjectUsingPrefabAsModel(damage, 9, .2f, false, Util.Enums.Direction.None, 10, 0, true, node.GetComponent<GridNode>(), actor);
+                }
+            }
+        }
+    }
+}
